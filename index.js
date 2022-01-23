52ab9561b015010ba9013e812da208860503a982
Tamoy <sinclairtamoy@gmail.com>
2022-01-23 00:27:51 -0500

    commit tree 8af336b71721e509bc17457799eb0486160a13b6

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 1
+let num2 = 62
+
+const multiply = num1 * num2 
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 6
+let num4 = 10
+
+let mod = num4%num3
+
+let max = 
+Math.max(5, 10, 15, 20)
